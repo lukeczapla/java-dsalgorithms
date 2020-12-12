9cdf62ff9f429b160b753db6f5d40cb79ddec524
Luke Czapla <luke@Lukes-MBP.fios-router.home>
2020-12-12 14:06:51 -0500

    Heap update

--- Heap.java
+++ Heap.java
@@ -1,15 +1,16 @@
-package edu.czapla.reactdemo.misc;
 
+import java.io.Serializable;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.ArrayList;
 
-public class Heap<T> {
+public class Heap<T> implements Cloneable, Serializable {
 
+	static final long serialVersionUID = -12388768391420L;
 	private List<T> elements;
 	private Comparator<T> comparator;
-
+	
 	public Heap() {
 	  this.elements = new ArrayList<>();
 	  this.comparator = Comparator.comparingInt(T::hashCode);
@@ -30,10 +31,7 @@ public class Heap<T> {
 		int index = elements.size()-1;
 		while (index != 0 && comparator.compare(elements.get(index), elements.get((index-1)/2)) < 0) {
 			int parentIndex = (index-1)/2;
-			Collections.swap(elements, index, parentIndex);
-//			T temp = elements.get(parentIndex);
-//			elements.set(parentIndex, elements.get(index));
-//			elements.set(index, temp);
+			Collections.swap(elements, parentIndex, index);
 			index = parentIndex;
 		}
 	}
@@ -50,10 +48,9 @@ public class Heap<T> {
 				int smallerIndex = 2*index+1;
 				if (2*index+2 < elements.size() && comparator.compare(elements.get(2*index+2), elements.get(2*index+1)) < 0)
 					smallerIndex = 2*index+2;
-				Collections.swap(elements, index, smallerIndex);
-//				T temp = elements.get(index);
-//				elements.set(index, elements.get(smallerIndex));
-//				elements.set(smallerIndex, temp);
+				T temp = elements.get(index);
+				elements.set(index, elements.get(smallerIndex));
+				elements.set(smallerIndex, temp);
 				index = smallerIndex;
 			}
 		}
@@ -62,6 +59,21 @@ public class Heap<T> {
 
 	public int size() {
 		return elements.size();
+	}
+
+	@SuppressWarnings("unchecked")
+	public Heap<T> clone() throws CloneNotSupportedException {
+		Heap<T> copy = (Heap<T>)super.clone();
+		copy.elements = new ArrayList<>(this.elements);
+		return copy;
+	}
+
+	public void setComparator(Comparator<T> comparator) {
+		List<T> values = elements;
+		this.comparator = comparator;
+		elements = new ArrayList<>(values.size());
+		// recreate the heap with new comparator
+		while (!values.isEmpty()) add(values.remove(0));
 	}
 	
 }
